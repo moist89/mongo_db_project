140b3c8f4f8ed73faf30f39804cae35bc4332d55
moises <moist.medina@gmail.com>
2025-06-09 16:55:52 -0400

    intial commit

---
+++ src/test/java/com/tutorial/mongo_db_project/MongoDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.mongo_db_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
